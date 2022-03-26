e64d988d445bf8114d6617803296834793c0a870
Juanjogit config --global user.name Juanjogit config --global user.name Juanjo <juanjo.marincastillo@gmail.com>
2022-03-26 07:38:27 +0100

    26-03

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import { loadStripe } from '@stripe/stripe-js';
-import { Elements, CardElement } from '@stripe/react-stripe-js';
-
+import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import "bootswatch/dist/lux/bootstrap.min.css";
+import lijadora from './lijadora-pulidora.jpeg';
+//import './App.css';
 
 const stripePromise = loadStripe("pk_test_51KhCFgJ6bLeWbu1kZmWlqQ8i9Q7yLXlqiLohUaedj3GYFXIQs5Z130udKnObvgyb20sFM88rayFPEO6rFCHQuKks005vUJR1wA");
 const CheckoutForm = ()=>{
-  return <form>
+  const stripe = useStripe();
+  const elements = useElements();
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type:'card',
+      card: elements.getElement(CardElement)
+    })
+  }
+  
+
+  return <form onSubmit={handleSubmit} >
+    
+    <img 
+      src={lijadora}
+      alt="lijadora" 
+      className='img-fluid'
+    />    
     <CardElement/>
+    <button>
+      Buy
+    </button>
   </form>
 }
 function App() {
   return (
     <Elements stripe={stripePromise}>
-      <CheckoutForm/>
+      <div className='container p-4'>
+        <div className='row'>
+          <div className='.col-md-4 offset-md-4'>
+            <CheckoutForm/>
+          </div>
+        </div>
+      </div>
     </Elements>
   );
 }
